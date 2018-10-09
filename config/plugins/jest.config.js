1896ddeb67a42b9e918372b8d00fbbee4ad81643
Amy Pellegrini <amy@digital-detox.co.uk>
2018-10-09 16:11:22 +0100

    Save progress

---
+++ config/plugins/jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  testPathIgnorePatterns: ["/integration-tests/", "/unit-tests/"],
+  rootDir: "../../plugins"
+};
--- pages/tests/integration-tests/login.test.js
+++ pages/tests/integration-tests/login.test.js
@@ -3,8 +3,8 @@ import toMatchScreenshot from "../../../plugins/toMatchScreenshot";
 // TODO: this could be abstracted even further via Jest config
 expect.extend({ toMatchScreenshot });
 
-describe("Index/home route", () => {
+describe("Other page route", () => {
   it("should match previous screenshot", async () => {
-    await expect("http://localhost:3000").toMatchScreenshot();
+    await expect("http://localhost:3000/login").toMatchScreenshot();
   });
 });
